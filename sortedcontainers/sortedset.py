f5751693eddb27de5468828151dbf08faf0b5c8c
Grant Jenks <grant.jenks@gmail.com>
2014-01-24 11:37:06 -0800

    Update sorted set impl to use built-in set

--- sortedcontainers/sortedset.py
+++ sortedcontainers/sortedset.py
@@ -4,131 +4,163 @@ Sorted set implementation.
 
 from sortedlist import SortedList
 from collections import MutableSet, Sequence
-from itertools import izip
+from itertools import izip, chain
 
 class SortedSet(MutableSet, Sequence):
     def __init__(self, iterable=None, load=100):
-        self._slist = SortedList(iterable, load)
+        self._set = set()
+        self._list = SortedList(load=load)
         if iterable is not None:
             self.update(iterable)
     def __contains__(self, value):
-        return (value in self._slist)
+        return (value in self._set)
     def __getitem__(self, index):
         if isinstance(index, slice):
-            start, stop, step, indices = self._slist._slice_indices(index)
-            return SortedSet(self._slist[index] for index in indices)
+            return SortedSet(self._list[index])
         else:
-            return self._slist[index]
+            return self._list[index]
     def __delitem__(self, index):
-        del self._slist[index]
+        if isinstance(index, slice):
+            values = self._list[index]
+            self._set.difference_update(values)
+        else:
+            value = self._list[index]
+            self._set.remove(value)
+        del self._list[index]
     def __setitem__(self, index, value):
-        self._slist[index] = value
+        if isinstance(index, slice):
+            values = self._list[index]
+            self._set.difference_update(values)
+            self._set.update(value)
+        else:
+            value = self._list[index]
+            self._set.remove(value)
+            self._set.add(value)
+        self._list[index] = value
     def __eq__(self, that):
         if len(self) != len(that):
             return False
         if isinstance(that, SortedSet):
             return all(lhs == rhs for lhs, rhs in izip(self, that))
+        elif isinstance(that, set):
+            return (self._set == that)
         else:
-            return all((val in self) for val in that)
+            return all(val in self._set for val in that)
     def __ne__(self, that):
         if len(self) != len(that):
             return True
         if isinstance(that, SortedSet):
             return any(lhs != rhs for lhs, rhs in izip(self, that))
+        elif isinstance(that, set):
+            return (self._set != that)
         else:
-            return any((val not in self) for val in that)
+            return any(val not in self._set for val in that)
     def __lt__(self, that):
-        return ((len(self._slist) < len(that))
-                and all((value in that) for value in self._slist))
+        if isinstance(that, set):
+            return (self._set < that)
+        else:
+            return (len(self) < len(that)) and all(val in that for val in self._list)
     def __gt__(self, that):
-        return ((len(self._slist) > len(that))
-                and all((value in self._slist) for value in that))
+        if isinstance(that, set):
+            return (self._set > that)
+        else:
+            return (len(self) > len(that)) and all(val in self._set for val in that)
     def __le__(self, that):
-        return all((value in that) for value in self._slist)
+        if isinstance(that, set):
+            return (self._set <= that)
+        else:
+            return all(val in that for val in self._list)
     def __ge__(self, that):
-        return all((value in self._slist) for value in that)
+        if isinstance(that, set):
+            return (self._set >= that)
+        else:
+            return all(val in self._set for val in that)
     def __iter__(self):
-        return iter(self._slist)
+        return iter(self._list)
     def __len__(self):
-        return len(self._slist)
+        return len(self._set)
     def reversed(self):
-        return reversed(self._slist)
+        return reversed(self._list)
     def add(self, value):
-        if value not in self._slist:
-            self._slist.add(value)
+        if value not in self._set:
+            self._set.add(value)
+            self._list.add(value)
     def bisect_left(self, value):
-        return self._slist.bisect_left(value)
+        return self._list.bisect_left(value)
     def bisect(self, value):
-        return self._slist.bisect(value)
+        return self._list.bisect(value)
     def bisect_right(self, value):
-        return self._slist.bisect_right(value)
+        return self._list.bisect_right(value)
     def clear(self):
-        self._slist.clear()
+        self._set.clear()
+        self._list.clear()
     def copy(self):
-        return SortedSet(self._slist)
+        return SortedSet(self._list)
     def count(self, value):
-        return self._slist.count(value)
+        return 1 if value in self._set else 0
+    def discard(self, value):
+        if value in self._set:
+            self._set.remove(value)
+            self._list.discard(value)
+    def index(self, value, start=None, stop=None):
+        return self._list.index(value, start, stop)
+    def isdisjoint(self, that):
+        return self._set.isdisjoint(that)
+    def issubset(self, that):
+        return self._set.issubset(that)
+    def issuperset(self, that):
+        return self._set.issuperset(that)
+    def pop(self, index=-1):
+        value = self._list.pop(index)
+        self._set.remove(value)
+        return value
+    def remove(self, value):
+        self._set.remove(value)
+        self._list.remove(value)
     def difference(self, *iterables):
         new_set = self.copy()
         new_set.difference_update(*iterables)
         return new_set
     def difference_update(self, *iterables):
-        for iterable in iterables:
-            for value in iterable:
+        values = set(chain(*iterables))
+        if (2 * len(values)) > len(self):
+            self._set.difference_update(values)
+            self._list.clear()
+            self._list.update(self._set)
+        else:
+            for value in values:
                 self.discard(value)
-    def discard(self, value):
-        self._slist.discard(value)
-    def index(self, value, start=None, stop=None):
-        return self._slist.index(value, start, stop)
     def intersection(self, *iterables):
         new_set = self.copy()
         new_set.intersection_update(*iterables)
         return new_set
     def intersection_update(self, *iterables):
-        new_list = SortedList()
-        for iterable in iterables:
-            for value in iterable:
-                if value in self._slist:
-                    if value not in new_list:
-                        new_list.add(value)
-            self._slist = new_list
-            new_list = SortedList()
-    def isdisjoint(self, that):
-        return not any((value in self._slist) for value in that)
-    def issubset(self, that):
-        return all((value in that) for value in self._slist)
-    def issuperset(self, that):
-        return all((value in self._slist) for value in that)
+        self._set.intersection_update(*iterables)
+        self._list.clear()
+        self._list.update(self._set)
     def symmetric_difference(self, that):
         new_set = self.copy()
         new_set.symmetric_difference_update(that)
         return new_set
     def symmetric_difference_update(self, that):
-        new_list = SortedList()
-        for value in self:
-            if value not in that:
-                new_list.add(value)
-        for value in that:
-            if value not in self:
-                new_list.add(value)
-        self._slist = new_list
-    def pop(self, index=-1):
-        return self._slist.pop(index)
-    def remove(self, value):
-        self._slist.remove(value)
+        self._set.symmetric_difference_update(that)
+        self._list.clear()
+        self._list.update(self._set)
     def union(self, *iterables):
-        new_set = self.copy()
-        new_set.update(*iterables)
-        return new_set
+        return SortedSet(chain(iter(self), *iterables), load=self._list._load)
     def update(self, *iterables):
-        for iterable in iterables:
-            for value in iterable:
-                if value not in self._slist:
-                    self._slist.add(value)
+        """Update sorted set with iterables."""
+        values = set(chain(*iterables))
+        if (2 * len(values)) > len(self):
+            self._set.update(values)
+            self._list.clear()
+            self._list.update(self._set)
+        else:
+            for value in values:
+                self.add(value)
     def __repr__(self):
-        reprs = (repr(value) for value in self)
-        return 'SortedSet([{}])'.format(', '.join(reprs))
+        return 'SortedSet({0})'.format(repr(list(self)))
     def _check(self):
-        self._slist._check()
-        assert all(self[pos - 1] != self[pos]
-                   for pos in xrange(1, len(self)))
+        self._list._check()
+        assert len(self._set) == len(self._list)
+        assert all(val in self._set for val in self._list)
