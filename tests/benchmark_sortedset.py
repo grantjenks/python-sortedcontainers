6bec8a7e7e4cd0d74db7332bf945fe48599d918d
Grant Jenks <grant.jenks@gmail.com>
2014-01-27 12:16:33 -0800

    Add benchmarks for sortedsets

--- tests/benchmark_sortedset.py
+++ tests/benchmark_sortedset.py
@@ -23,6 +23,144 @@ def add(func, size):
     for val in lists[size]:
         func(val)
 
+@register_test
+def update_tiny(func, size):
+    func(xrange(size, size + 10))
+
+@register_test
+def update_small(func, size):
+    func(xrange(size, size + size / 10))
+
+@register_test
+def update_medium(func, size):
+    func(xrange(size, size + size * 5 / 10))
+
+@register_test
+def update_large(func, size):
+    func(xrange(size, size + size * 9 / 10))
+
+@register_test
+def union_tiny(func, size):
+    func(xrange(size, size + 10))
+
+@register_test
+def union_small(func, size):
+    func(xrange(size, size + size / 10))
+
+@register_test
+def union_medium(func, size):
+    func(xrange(size, size + size * 5 / 10))
+
+@register_test
+def union_large(func, size):
+    func(xrange(size, size + size * 9 / 10))
+
+@register_test
+def remove(func, size):
+    for val in lists[size]:
+        func(val)
+
+@register_test
+def difference_tiny(func, size):
+    func(xrange(size / 2, size / 2 + 10))
+
+@register_test
+def difference_small(func, size):
+    func(xrange(size / 2, size / 2 + size / 10))
+
+@register_test
+def difference_medium(func, size):
+    func(xrange(size / 2, size / 2 + size * 5 / 10))
+
+@register_test
+def difference_large(func, size):
+    func(xrange(size / 2, size / 2 + size * 9 / 10))
+
+@register_test
+def difference_update_tiny(func, size):
+    func(xrange(size / 2, size / 2 + 10))
+
+@register_test
+def difference_update_small(func, size):
+    func(xrange(size / 2, size / 2 + size / 10))
+
+@register_test
+def difference_update_medium(func, size):
+    func(xrange(size / 2, size / 2 + size * 5 / 10))
+
+@register_test
+def difference_update_large(func, size):
+    func(xrange(size / 2, size / 2 + size * 9 / 10))
+
+@register_test
+def intersection_tiny(func, size):
+    func(xrange(size / 2, size / 2 + 10))
+
+@register_test
+def intersection_small(func, size):
+    func(xrange(size / 2, size / 2 + size / 10))
+
+@register_test
+def intersection_medium(func, size):
+    func(xrange(size / 2, size / 2 + size * 5 / 10))
+
+@register_test
+def intersection_large(func, size):
+    func(xrange(size / 2, size / 2 + size * 9 / 10))
+
+@register_test
+def intersection_update_tiny(func, size):
+    func(xrange(size / 2, size / 2 + 10))
+
+@register_test
+def intersection_update_small(func, size):
+    func(xrange(size / 2, size / 2 + size / 10))
+
+@register_test
+def intersection_update_medium(func, size):
+    func(xrange(size / 2, size / 2 + size * 5 / 10))
+
+@register_test
+def intersection_update_large(func, size):
+    func(xrange(size / 2, size / 2 + size * 9 / 10))
+
+@register_test
+def symmetric_difference_tiny(func, size):
+    func(xrange(size / 2, size / 2 + 10))
+
+@register_test
+def symmetric_difference_small(func, size):
+    func(xrange(size / 2, size / 2 + size / 10))
+
+@register_test
+def symmetric_difference_medium(func, size):
+    func(xrange(size / 2, size / 2 + size * 5 / 10))
+
+@register_test
+def symmetric_difference_large(func, size):
+    func(xrange(size / 2, size / 2 + size * 9 / 10))
+
+@register_test
+def symmetric_difference_update_tiny(func, size):
+    func(xrange(size / 2, size / 2 + 10))
+
+@register_test
+def symmetric_difference_update_small(func, size):
+    func(xrange(size / 2, size / 2 + size / 10))
+
+@register_test
+def symmetric_difference_update_medium(func, size):
+    func(xrange(size / 2, size / 2 + size * 5 / 10))
+
+@register_test
+def symmetric_difference_update_large(func, size):
+    func(xrange(size / 2, size / 2 + size * 9 / 10))
+
+@register_test
+def pop(func, size):
+    for rpt in xrange(size):
+        func()
+
 # Setups.
 
 def do_nothing(obj, size):
@@ -69,6 +207,288 @@ for name, kind in kinds.items():
         'setup': do_nothing,
         'ctor': kind,
         'func': 'add'
+    }
+
+for name, kind in kinds.items():
+    impls['update_tiny'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'update'
+    }
+
+for name, kind in kinds.items():
+    impls['update_small'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'update'
+    }
+
+for name, kind in kinds.items():
+    impls['update_medium'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'update'
+    }
+
+for name, kind in kinds.items():
+    impls['update_large'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'update'
+    }
+
+del impls['update_tiny']['SkipListSet']
+del impls['update_small']['SkipListSet']
+del impls['update_medium']['SkipListSet']
+del impls['update_large']['SkipListSet']
+
+for name, kind in kinds.items():
+    impls['union_tiny'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'union'
+    }
+
+for name, kind in kinds.items():
+    impls['union_small'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'union'
+    }
+
+for name, kind in kinds.items():
+    impls['union_medium'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'union'
+    }
+
+for name, kind in kinds.items():
+    impls['union_large'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'union'
+    }
+
+del impls['union_tiny']['SkipListSet']
+del impls['union_small']['SkipListSet']
+del impls['union_medium']['SkipListSet']
+del impls['union_large']['SkipListSet']
+
+for name, kind in kinds.items():
+    impls['remove'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'remove'
+    }
+
+for name, kind in kinds.items():
+    impls['difference_tiny'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'difference'
+    }
+
+for name, kind in kinds.items():
+    impls['difference_small'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'difference'
+    }
+
+for name, kind in kinds.items():
+    impls['difference_medium'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'difference'
+    }
+
+for name, kind in kinds.items():
+    impls['difference_large'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'difference'
+    }
+
+del impls['difference_tiny']['SkipListSet']
+del impls['difference_small']['SkipListSet']
+del impls['difference_medium']['SkipListSet']
+del impls['difference_large']['SkipListSet']
+
+for name, kind in kinds.items():
+    impls['difference_update_tiny'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'difference_update'
+    }
+
+for name, kind in kinds.items():
+    impls['difference_update_small'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'difference_update'
+    }
+
+for name, kind in kinds.items():
+    impls['difference_update_medium'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'difference_update'
+    }
+
+for name, kind in kinds.items():
+    impls['difference_update_large'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'difference_update'
+    }
+
+del impls['difference_update_tiny']['SkipListSet']
+del impls['difference_update_small']['SkipListSet']
+del impls['difference_update_medium']['SkipListSet']
+del impls['difference_update_large']['SkipListSet']
+
+for name, kind in kinds.items():
+    impls['intersection_tiny'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'intersection'
+    }
+
+for name, kind in kinds.items():
+    impls['intersection_small'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'intersection'
+    }
+
+for name, kind in kinds.items():
+    impls['intersection_medium'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'intersection'
+    }
+
+for name, kind in kinds.items():
+    impls['intersection_large'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'intersection'
+    }
+
+del impls['intersection_tiny']['SkipListSet']
+del impls['intersection_small']['SkipListSet']
+del impls['intersection_medium']['SkipListSet']
+del impls['intersection_large']['SkipListSet']
+
+for name, kind in kinds.items():
+    impls['intersection_update_tiny'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'intersection_update'
+    }
+
+for name, kind in kinds.items():
+    impls['intersection_update_small'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'intersection_update'
+    }
+
+for name, kind in kinds.items():
+    impls['intersection_update_medium'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'intersection_update'
+    }
+
+for name, kind in kinds.items():
+    impls['intersection_update_large'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'intersection_update'
+    }
+
+del impls['intersection_update_tiny']['SkipListSet']
+del impls['intersection_update_small']['SkipListSet']
+del impls['intersection_update_medium']['SkipListSet']
+del impls['intersection_update_large']['SkipListSet']
+
+for name, kind in kinds.items():
+    impls['symmetric_difference_tiny'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'symmetric_difference'
+    }
+
+for name, kind in kinds.items():
+    impls['symmetric_difference_small'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'symmetric_difference'
+    }
+
+for name, kind in kinds.items():
+    impls['symmetric_difference_medium'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'symmetric_difference'
+    }
+
+for name, kind in kinds.items():
+    impls['symmetric_difference_large'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'symmetric_difference'
+    }
+
+del impls['symmetric_difference_tiny']['SkipListSet']
+del impls['symmetric_difference_small']['SkipListSet']
+del impls['symmetric_difference_medium']['SkipListSet']
+del impls['symmetric_difference_large']['SkipListSet']
+
+for name, kind in kinds.items():
+    impls['symmetric_difference_update_tiny'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'symmetric_difference_update'
+    }
+
+for name, kind in kinds.items():
+    impls['symmetric_difference_update_small'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'symmetric_difference_update'
+    }
+
+for name, kind in kinds.items():
+    impls['symmetric_difference_update_medium'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'symmetric_difference_update'
+    }
+
+for name, kind in kinds.items():
+    impls['symmetric_difference_update_large'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'symmetric_difference_update'
+    }
+
+del impls['symmetric_difference_update_tiny']['rbset']
+del impls['symmetric_difference_update_small']['rbset']
+del impls['symmetric_difference_update_medium']['rbset']
+del impls['symmetric_difference_update_large']['rbset']
+del impls['symmetric_difference_update_tiny']['SkipListSet']
+del impls['symmetric_difference_update_small']['SkipListSet']
+del impls['symmetric_difference_update_medium']['SkipListSet']
+del impls['symmetric_difference_update_large']['SkipListSet']
+
+for name, kind in kinds.items():
+    impls['pop'][name] = {
+        'setup': fill_values,
+        'ctor': kind,
+        'func': 'pop'
     }
 
 if __name__ == '__main__':
