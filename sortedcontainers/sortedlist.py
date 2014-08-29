dac6a7205a5fd1fe3fad0d98870c18143eb764b8
Grant Jenks <contact@grantjenks.com>
2014-08-29 11:33:03 -0700

    Add comments for new index scheme

--- sortedcontainers/sortedlist.py
+++ sortedcontainers/sortedlist.py
@@ -102,6 +102,12 @@ class SortedList(MutableSequence):
         self._len += 1
 
     def _expand(self, pos):
+        """Splits sublists that are more than double the load level.
+
+        Updates the index when the sublist length is less than double the load
+        level. This requires incrementing the nodes in a traversal from the leaf
+        node to the root. For an example traversal see self._loc.
+        """
         _lists, _index = self._lists, self._index
 
         if len(_lists[pos]) > self._twice:
@@ -205,7 +211,13 @@ class SortedList(MutableSequence):
 
     def _delete(self, pos, idx):
         """Delete the item at the given (pos, idx).
-        Combines lists that are less than half the load level."""
+
+        Combines lists that are less than half the load level.
+
+        Updates the index when the sublist length is more than half the load
+        level. This requires decrementing the nodes in a traversal from the leaf
+        node to the root. For an example traversal see self._loc.
+        """
         _maxes, _lists, _index = self._maxes, self._lists, self._index
 
         lists_pos = _lists[pos]
@@ -251,6 +263,49 @@ class SortedList(MutableSequence):
             del _index[:]
 
     def _loc(self, pos, idx):
+        """Convert an index pair (alpha, beta) into a single index that corresponds to
+        the position of the value in the sorted list.
+
+        Most queries require the index be built. Details of the index are
+        described in self._build_index.
+
+        Indexing requires traversing the tree from a leaf node to the root. The parent
+        of each node is easily computable at (pos - 1) // 2.
+
+        Left-child nodes are always at odd indices and right-child nodes are always
+        at even indices.
+
+        When traversing up from a right-child node, increment the total by the
+        left-child node.
+
+        The final index is the sum from traversal and the index in the sublist.
+
+        For example, using the index from self._build_index:
+
+        _index = 14 5 9 3 2 4 5
+        _offset = 3
+
+        Tree:
+
+                 14
+              5      9
+            3   2  4   5
+
+        Converting index pair (2, 3) into a single index involves iterating like
+        so:
+
+        1. Starting at the leaf node: offset + alpha = 3 + 2 = 5. We identify
+           the node as a left-child node. At such nodes, we simply traverse to
+           the parent.
+
+        2. At node 9, position 2, we recognize the node as a right-child node
+           and accumulate the left-child in our total. Total is now 5 and we
+           traverse to the parent at position 0.
+
+        3. Iteration ends at the root.
+
+        Computing the index is the sum of the total and beta: 5 + 3 = 8.
+        """
         if pos == 0:
             return idx
 
@@ -282,6 +337,55 @@ class SortedList(MutableSequence):
         return total + idx
 
     def _pos(self, idx):
+        """Convert an index into a pair (alpha, beta) that can be used to access
+        the corresponding _lists[alpha][beta] position.
+
+        Most queries require the index be built. Details of the index are
+        described in self._build_index.
+
+        Indexing requires traversing the tree to a leaf node. Each node has
+        two children which are easily computable. Given an index, pos, the
+        left-child is at pos * 2 + 1 and the right-child is at pos * 2 + 2.
+
+        When the index is less than the left-child, traversal moves to the
+        left sub-tree. Otherwise, the index is decremented by the left-child
+        and traversal moves to the right sub-tree.
+
+        At a child node, the indexing pair is computed from the relative
+        position of the child node as compared with the offset and the remaining
+        index.
+
+        For example, using the index from self._build_index:
+
+        _index = 14 5 9 3 2 4 5
+        _offset = 3
+
+        Tree:
+
+                 14
+              5      9
+            3   2  4   5
+
+        Indexing position 8 involves iterating like so:
+
+        1. Starting at the root, position 0, 8 is compared with the left-child
+           node (5) which it is greater than. When greater the index is
+           decremented and the position is updated to the right child node.
+
+        2. At node 9 with index 3, we again compare the index to the left-child
+           node with value 4. Because the index is the less than the left-child
+           node, we simply traverse to the left.
+
+        3. At node 4 with index 3, we recognize that we are at a leaf node and
+           stop iterating.
+
+        4. To compute the sublist index, we subtract the offset from the index
+           of the leaf node: 5 - 3 = 2. To compute the index in the sublist, we
+           simply use the index remaining from iteration. In this case, 3.
+
+        The final index pair from our example is (2, 3) which corresponds to
+        index 8 in the sorted list.
+        """
         _len, _lists = self._len, self._lists
 
         if idx < 0:
@@ -304,22 +408,55 @@ class SortedList(MutableSequence):
 
         pos = 0
         len_index = len(_index)
+        child = (pos << 1) + 1
 
-        while True:
+        while child < len_index:
+            index_child = _index[child]
+
+            if idx < index_child:
+                pos = child
+            else:
+                idx -= index_child
+                pos = child + 1
+
             child = (pos << 1) + 1
 
-            if child < len_index:
-                index_child = _index[child]
-
-                if idx < index_child:
-                    pos = child
-                else:
-                    idx -= index_child
-                    pos = child + 1
-            else:
-                return (pos - self._offset, idx)
+        return (pos - self._offset, idx)
 
     def _build_index(self):
+        """Build an index for indexing the sorted list.
+
+        Indexes are represented as binary trees in a dense array notation
+        similar to a binary heap.
+
+        For example, given a _lists representation storing integers:
+
+        [0]: 1 2 3
+        [1]: 4 5
+        [2]: 6 7 8 9
+        [3]: 10 11 12 13 14
+
+        The first transformation maps the sub-lists by their length. The
+        first row of the index is the length of the sub-lists.
+
+        [0]: 3 2 4 5
+
+        Each row after that is the sum of consecutive pairs of the previous row:
+
+        [1]: 5 9
+        [2]: 14
+
+        Finally, the index is built by concatenating these lists together:
+
+        _index = 14 5 9 3 2 4 5
+
+        An offset storing the start of the first row is also stored:
+
+        _offset = 3
+
+        When built, the index can be used for efficient indexing into the list.
+        See the comment and notes on self._pos for details.
+        """
         row0 = list(map(len, self._lists))
 
         if len(row0) == 1:
