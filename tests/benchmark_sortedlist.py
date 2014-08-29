f1781e6b1d51add49be3921fc6a008288af28025
Grant Jenks <contact@grantjenks.com>
2014-08-29 13:19:45 -0700

    Add mixed-mode benchmarks for sortedlist: PriorityQueue, Multiset, Ranking, Neighbor, Intervals

--- tests/benchmark_sortedlist.py
+++ tests/benchmark_sortedlist.py
@@ -65,6 +65,31 @@ def count(func, size):
     for val in lists[size][::100]:
         assert func(val) == 1
 
+@register_test
+def priorityqueue(func, size):
+    for val in lists[size][::10]:
+        func(val)
+
+@register_test
+def multiset(func, size):
+    for val in lists[size][::10]:
+        func(val)
+
+@register_test
+def ranking(func, size):
+    for val in lists[size][::10]:
+        func(val)
+
+@register_test
+def neighbor(func, size):
+    for val in lists[size][::10]:
+        func(val)
+
+@register_test
+def intervals(func, size):
+    for val in lists[size][::10]:
+        func(val)
+
 # Setups.
 
 def do_nothing(obj, size):
@@ -183,6 +208,178 @@ for name, kind in kinds.items():
         'setup': fill_values,
         'ctor': kind,
         'func': 'count',
+        'limit': 1000000
+    }
+
+class Mixed:
+    def __init__(self, kind):
+        self.rand = random.Random(0)
+        self.kind = kind
+    def __call__(self, *args, **kwargs):
+        self.obj = self.kind(*args, **kwargs)
+        return self
+    def update(self, values):
+        self.obj.update(values)
+    def run(self, value):
+        raise NotImplementedError
+
+class PriorityQueue(Mixed):
+    def run(self, value):
+        """
+        40% add
+        40% pop
+        10% discard
+        9% contains
+        1% iter (first 100 elements)
+        """
+        obj = self.obj
+        pos = self.rand.randrange(100)
+
+        if pos < 40:
+            obj.add(value)
+        elif pos < 80:
+            obj.pop()
+        elif pos < 90:
+            obj.discard(value)
+        elif pos < 99:
+            value in obj
+        else:
+            for idx, temp in enumerate(obj):
+                if idx > 100:
+                    break
+
+for name, kind in kinds.items():
+    impls['priorityqueue'][name] = {
+        'setup': fill_values,
+        'ctor': PriorityQueue(kind),
+        'func': 'run',
+        'limit': 1000000
+    }
+
+class Multiset(Mixed):
+    def run(self, value):
+        """
+        75% contains
+        10% add
+        10% remove
+        5% getitem
+        """
+        obj = self.obj
+        pos = self.rand.randrange(100)
+
+        if pos < 75:
+            assert value in obj
+        elif pos < 85:
+            obj.add(value)
+        elif pos < 95:
+            obj.remove(value)
+        else:
+            if value < len(obj):
+                obj[value]
+
+for name, kind in kinds.items():
+    impls['multiset'][name] = {
+        'setup': fill_values,
+        'ctor': Multiset(kind),
+        'func': 'run',
+        'limit': 1000000
+    }
+
+class Ranking(Mixed):
+    def run(self, value):
+        """
+        40% getitem
+        40% index
+        10% add
+        10% remove
+        """
+        obj = self.obj
+        pos = self.rand.randrange(100)
+
+        if pos < 40:
+            if value < len(obj):
+                obj[value]
+        elif pos < 80:
+            assert obj.index(value) >= 0
+        elif pos < 90:
+            obj.add(value)
+        else:
+            obj.remove(value)
+
+for name, kind in kinds.items():
+    impls['ranking'][name] = {
+        'setup': fill_values,
+        'ctor': Ranking(kind),
+        'func': 'run',
+        'limit': 1000000
+    }
+
+class Neighbor(Mixed):
+    def run(self, value):
+        """
+        75% bisect
+        10% add
+        10% remove
+        5% iter (first 100 elements)
+        """
+        obj = self.obj
+        pos = self.rand.randrange(100)
+
+        if pos < 75:
+            obj.bisect(value)
+        elif pos < 85:
+            obj.add(value)
+        elif pos < 95:
+            obj.remove(value)
+        else:
+            for idx, temp in enumerate(obj):
+                if idx > 100:
+                    break
+
+for name, kind in kinds.items():
+    impls['neighbor'][name] = {
+        'setup': fill_values,
+        'ctor': Neighbor(kind),
+        'func': 'run',
+        'limit': 1000000
+    }
+
+class Intervals(Mixed):
+    def run(self, value):
+        """
+        30% bisect
+        20% getitem
+        20% delitem
+        10% get-slice (range query)
+        10% add
+        10% discard
+        """
+        obj = self.obj
+        pos = self.rand.randrange(100)
+
+        if pos < 30:
+            obj.bisect(value)
+        elif pos < 50:
+            if value < len(obj):
+                obj[value]
+        elif pos < 70:
+            if value < len(obj):
+                del obj[value]
+        elif pos < 80:
+            if value < len(obj):
+                limit = min(value + 100, len(obj))
+                other = self.rand.randrange(value, limit)
+                obj[value:other]
+        elif pos < 90:
+            obj.add(value)
+        else:
+            obj.discard(value)
+
+for name, kind in kinds.items():
+    impls['intervals'][name] = {
+        'setup': fill_values,
+        'ctor': Intervals(kind),
+        'func': 'run',
         'limit': 1000000
     }
 
