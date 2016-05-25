71a5f15d567327e32488fc22f07ae4b58420f768
Grant Jenks <contact@grantjenks.com>
2016-05-25 12:53:53 -0700

    Add imgmath Sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinx.ext.imgmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
