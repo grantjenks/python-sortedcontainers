7f2d85c83fc394b34f6e1898e42c52ccb735c41f
Grant Jenks <contact@grantjenks.com>
2016-05-26 10:20:40 -0700

    Bump version to 1.5.0

--- sortedcontainers/__init__.py
+++ sortedcontainers/__init__.py
@@ -41,8 +41,8 @@ testing has 100% coverage with unit tests and hours of stress.
 """
 
 __title__ = 'sortedcontainers'
-__version__ = '1.4.4'
-__build__ = 0x010404
+__version__ = '1.5.0'
+__build__ = 0x010500
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Grant Jenks'
