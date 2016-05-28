c9b625d7ee1e6516500ec3d45f4efc0ce2b12be7
Grant Jenks <contact@grantjenks.com>
2016-05-27 17:01:57 -0700

    Remove size limits (from testing)

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -1,50 +1,50 @@
 # Compare Implementations
 
-echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --size 100 --size 1000 > tests/results_sortedlist.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare > tests/results_sortedlist.txt" | bash
 python -m tests.benchmark_plot tests/results_sortedlist.txt SortedList --save
 
-echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --size 100 --size 1000 > tests/results_sorteddict.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare > tests/results_sorteddict.txt" | bash
 python -m tests.benchmark_plot tests/results_sorteddict.txt SortedDict --save
 
-echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --size 100 --size 1000 > tests/results_sortedset.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare > tests/results_sortedset.txt" | bash
 python -m tests.benchmark_plot tests/results_sortedset.txt SortedSet --save
 
 # Compare Python Versions
 
 rm tests/results_runtime_sortedlist.txt
-echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --size 100 --size 1000 --kind SortedList --suffix _Py27 >> tests/results_runtime_sortedlist.txt" | bash
-echo ". env35/bin/activate && python -m tests.benchmark_sortedlist --bare --size 100 --size 1000 --kind SortedList --suffix _Py35 >> tests/results_runtime_sortedlist.txt" | bash
-echo ". env27/bin/activate && pypy -m tests.benchmark_sortedlist --bare --size 100 --size 1000 --kind SortedList --suffix _PyPy >> tests/results_runtime_sortedlist.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --kind SortedList --suffix _Py27 >> tests/results_runtime_sortedlist.txt" | bash
+echo ". env35/bin/activate && python -m tests.benchmark_sortedlist --bare --kind SortedList --suffix _Py35 >> tests/results_runtime_sortedlist.txt" | bash
+echo ". env27/bin/activate && pypy -m tests.benchmark_sortedlist --bare --kind SortedList --suffix _PyPy >> tests/results_runtime_sortedlist.txt" | bash
 python -m tests.benchmark_plot tests/results_runtime_sortedlist.txt SortedList --suffix _runtime --save
 
 rm tests/results_runtime_sorteddict.txt
-echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --size 100 --size 1000 --kind SortedDict --suffix _Py27 >> tests/results_runtime_sorteddict.txt" | bash
-echo ". env35/bin/activate && python -m tests.benchmark_sorteddict --bare --size 100 --size 1000 --kind SortedDict --suffix _Py35 >> tests/results_runtime_sorteddict.txt" | bash
-echo ". env27/bin/activate && pypy -m tests.benchmark_sorteddict --bare --size 100 --size 1000 --kind SortedDict --suffix _PyPy >> tests/results_runtime_sorteddict.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --kind SortedDict --suffix _Py27 >> tests/results_runtime_sorteddict.txt" | bash
+echo ". env35/bin/activate && python -m tests.benchmark_sorteddict --bare --kind SortedDict --suffix _Py35 >> tests/results_runtime_sorteddict.txt" | bash
+echo ". env27/bin/activate && pypy -m tests.benchmark_sorteddict --bare --kind SortedDict --suffix _PyPy >> tests/results_runtime_sorteddict.txt" | bash
 python -m tests.benchmark_plot tests/results_runtime_sorteddict.txt SortedDict --suffix _runtime --save
 
 rm tests/results_runtime_sortedset.txt
-echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --size 100 --size 1000 --kind SortedSet --suffix _Py27 >> tests/results_runtime_sortedset.txt" | bash
-echo ". env35/bin/activate && python -m tests.benchmark_sortedset --bare --size 100 --size 1000 --kind SortedSet --suffix _Py35 >> tests/results_runtime_sortedset.txt" | bash
-echo ". env27/bin/activate && pypy -m tests.benchmark_sortedset --bare --size 100 --size 1000 --kind SortedSet --suffix _PyPy >> tests/results_runtime_sortedset.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --kind SortedSet --suffix _Py27 >> tests/results_runtime_sortedset.txt" | bash
+echo ". env35/bin/activate && python -m tests.benchmark_sortedset --bare --kind SortedSet --suffix _Py35 >> tests/results_runtime_sortedset.txt" | bash
+echo ". env27/bin/activate && pypy -m tests.benchmark_sortedset --bare --kind SortedSet --suffix _PyPy >> tests/results_runtime_sortedset.txt" | bash
 python -m tests.benchmark_plot tests/results_runtime_sortedset.txt SortedSet --suffix _runtime --save
 
 # Compare Loads
 
 rm tests/results_load_sortedlist.txt
-echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --size 100 --size 1000 --kind SortedList --suffix _100 --load 100 --no-limit >> tests/results_load_sortedlist.txt" | bash
-echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --size 100 --size 1000 --kind SortedList --suffix _1000 --load 1000 --no-limit >> tests/results_load_sortedlist.txt" | bash
-echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --size 100 --size 1000 --kind SortedList --suffix _10000 --load 10000 --no-limit >> tests/results_load_sortedlist.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --kind SortedList --suffix _100 --load 100 --no-limit >> tests/results_load_sortedlist.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --kind SortedList --suffix _1000 --load 1000 --no-limit >> tests/results_load_sortedlist.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedlist --bare --kind SortedList --suffix _10000 --load 10000 --no-limit >> tests/results_load_sortedlist.txt" | bash
 python -m tests.benchmark_plot tests/results_load_sortedlist.txt SortedList --suffix _load --save
 
 rm tests/results_load_sorteddict.txt
-echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --size 100 --size 1000 --kind SortedDict --suffix _100 --load 100 --no-limit >> tests/results_load_sorteddict.txt" | bash
-echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --size 100 --size 1000 --kind SortedDict --suffix _1000 --load 1000 --no-limit >> tests/results_load_sorteddict.txt" | bash
-echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --size 100 --size 1000 --kind SortedDict --suffix _10000 --load 10000 --no-limit >> tests/results_load_sorteddict.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --kind SortedDict --suffix _100 --load 100 --no-limit >> tests/results_load_sorteddict.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --kind SortedDict --suffix _1000 --load 1000 --no-limit >> tests/results_load_sorteddict.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sorteddict --bare --kind SortedDict --suffix _10000 --load 10000 --no-limit >> tests/results_load_sorteddict.txt" | bash
 python -m tests.benchmark_plot tests/results_load_sorteddict.txt SortedDict --suffix _load --save
 
 rm tests/results_load_sortedset.txt
-echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --size 100 --size 1000 --kind SortedSet --suffix _100 --load 100 --no-limit >> tests/results_load_sortedset.txt" | bash
-echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --size 100 --size 1000 --kind SortedSet --suffix _1000 --load 1000 --no-limit >> tests/results_load_sortedset.txt" | bash
-echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --size 100 --size 1000 --kind SortedSet --suffix _10000 --load 10000 --no-limit >> tests/results_load_sortedset.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --kind SortedSet --suffix _100 --load 100 --no-limit >> tests/results_load_sortedset.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --kind SortedSet --suffix _1000 --load 1000 --no-limit >> tests/results_load_sortedset.txt" | bash
+echo ". env27/bin/activate && python -m tests.benchmark_sortedset --bare --kind SortedSet --suffix _10000 --load 10000 --no-limit >> tests/results_load_sortedset.txt" | bash
 python -m tests.benchmark_plot tests/results_load_sortedset.txt SortedSet --suffix _load --save
