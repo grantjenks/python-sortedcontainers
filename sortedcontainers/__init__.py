f6a359773de48e5adbe0e1946355d38f43c59c32
Grant Jenks <contact@grantjenks.com>
2016-10-16 13:41:28 -0700

    Bump version to 1.5.4

--- sortedcontainers/__init__.py
+++ sortedcontainers/__init__.py
@@ -45,8 +45,8 @@ from .sorteddict import SortedDict
 __all__ = ['SortedList', 'SortedSet', 'SortedDict', 'SortedListWithKey']
 
 __title__ = 'sortedcontainers'
-__version__ = '1.5.3'
-__build__ = 0x010503
+__version__ = '1.5.4'
+__build__ = 0x010504
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2016 Grant Jenks'
