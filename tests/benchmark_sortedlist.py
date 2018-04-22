298eb166044a19950524098e1caf4a154698438b
Grant Jenks <contact@grantjenks.com>
2018-04-22 15:20:06 -0700

    Change from remove(...) back to del

--- tests/benchmark_sortedlist.py
+++ tests/benchmark_sortedlist.py
@@ -208,7 +208,7 @@ for name, kind in kinds.items():
         'func': '__delitem__',
         'limit': 1000000
     }
-remove('delitem', 'sortedcollection')
+del impls['delitem']['sortedcollection']
 
 for name, kind in kinds.items():
     impls['bisect'][name] = {
@@ -433,7 +433,7 @@ for name, kind in kinds.items():
         'func': 'run',
         'limit': 1000000
     }
-remove('intervals', 'sortedcollection')
+del impls['intervals']['sortedcollection']
 
 for name, kind in kinds.items():
     impls['init'][name] = {
