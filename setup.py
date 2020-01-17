80e85f5970a5da2ba10cd5092fdd05a88947b5b8
Paul Ganssle <paul@ganssle.io>
2020-01-17 11:43:33 -0500

    Migrate to declarative build configuration
    The current best practice in Python packaging is to use delcarative
    build files in favor of setup.py wherever possible. This migrates
    nearly all the configuration from `setup.py` to `setup.cfg`.
    
    The only real change here is that the `name` attribute is no longer
    single-sourced, but since this seems very stable that seems like a
    reasonable trade-off.

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-import sortedcontainers
-
 
 class Tox(TestCommand):
     def finalize_options(self):
@@ -15,38 +13,7 @@ class Tox(TestCommand):
         exit(errno)
 
 
-with open('README.rst') as reader:
-    readme = reader.read()
-
 setup(
-    name=sortedcontainers.__title__,
-    version=sortedcontainers.__version__,
-    description='Sorted Containers -- Sorted List, Sorted Dict, Sorted Set',
-    long_description=readme,
-    author='Grant Jenks',
-    author_email='contact@grantjenks.com',
-    url='http://www.grantjenks.com/docs/sortedcontainers/',
-    license='Apache 2.0',
-    packages=['sortedcontainers'],
     tests_require=['tox'],
     cmdclass={'test': Tox},
-    install_requires=[],
-    classifiers=(
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ),
 )
